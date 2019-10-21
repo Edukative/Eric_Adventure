4ca020daac94465cec637e52ec34bb3c2db5e5d8
Edukative <56083723+Edukative@users.noreply.github.com>
2019-10-21 18:30:57 +0200

    Starting my new videogame
    I hope you like this video!! 100% Real No Fake, No Clickbait.

---
+++ Eric_Rubis_Adventour/Assets/Scripts/RubyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); //Get the horizontal input
+        float vertical = Input.GetAxis("Vertical"); //Get the vertical input
+
+        Vector2 position = transform.position; //Makes a vector based on current position :O
+
+        position.x = position.x + speed * horizontal * Time.deltaTime; //The position is equal to the same position but a little bit bigger XD
+        position.y = position.y + speed * vertical * Time.deltaTime; //Called each second instead of each frame
+
+        transform.position = position; //Saves the position to the current one =(
+
+        Debug.Log("horizontal" + horizontal); //See the values are you sending when pressing the key :))))
+        Debug.Log("vertical" + vertical);
+    }
+}
