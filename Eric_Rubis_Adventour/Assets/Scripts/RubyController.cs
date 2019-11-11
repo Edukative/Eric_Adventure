bdcb66de984899f85d112430b01467e9c3bbe327
Edukative <56083723+Edukative@users.noreply.github.com>
2019-11-11 18:32:03 +0100

    Los Angeles Laker
    NBA Championship

--- Eric_Rubis_Adventour/Assets/Scripts/RubyController.cs
+++ Eric_Rubis_Adventour/Assets/Scripts/RubyController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed;
+    Rigidbody2D rubyRB2D;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rubyRB2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -22,7 +24,8 @@ public class RubyController : MonoBehaviour
         position.x = position.x + speed * horizontal * Time.deltaTime; //The position is equal to the same position but a little bit bigger XD
         position.y = position.y + speed * vertical * Time.deltaTime; //Called each second instead of each frame
 
-        transform.position = position; //Saves the position to the current one =(
+        //transform.position = position; //Saves the position to the current one =(
+        rubyRB2D.MovePosition(position);
 
         Debug.Log("horizontal" + horizontal); //See the values are you sending when pressing the key :))))
         Debug.Log("vertical" + vertical);
