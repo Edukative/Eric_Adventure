5ae0f5c3e9eff1a57f6e11245e82ecd6ed57f0aa
Edukative <56083723+Edukative@users.noreply.github.com>
2019-11-25 18:31:46 +0100

    Health Collector
    "They are strawberries"

---
+++ Eric_Rubis_Adventour/Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("object that entered the trigger: " + other);
+
+        RubyController controller = other.GetComponent<RubyController>();
+        //Get the player controller from the other thing collided with the trigger :v
+        if(controller != null) //If the controller is not empty also, THE EXCLAMATION IS A NEGATION VALUE =O
+        {
+            controller.ChangeHealth(1); //Call the health function and add 1 to the health of the player
+            Destroy(gameObject); //Destroys all the game object ant this script to =!
+        }
+    }
+}
--- Eric_Rubis_Adventour/Assets/Scripts/RubyController.cs
+++ Eric_Rubis_Adventour/Assets/Scripts/RubyController.cs
@@ -6,11 +6,14 @@ public class RubyController : MonoBehaviour
 {
     public float speed;
     Rigidbody2D rubyRB2D;
-    
+    public int maxHealth = 5;
+    int currentHealth;
+
     // Start is called before the first frame update
     void Start()
     {
         rubyRB2D = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth; //The current health is the max health available to the player =D
     }
 
     // Update is called once per frame
@@ -19,7 +22,7 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal"); //Get the horizontal input
         float vertical = Input.GetAxis("Vertical"); //Get the vertical input
 
-        Vector2 position = transform.position; //Makes a vector based on current position :O
+        Vector2 position = transform.position; //Makes a vector based on current position =O
 
         position.x = position.x + speed * horizontal * Time.deltaTime; //The position is equal to the same position but a little bit bigger XD
         position.y = position.y + speed * vertical * Time.deltaTime; //Called each second instead of each frame
@@ -27,7 +30,13 @@ public class RubyController : MonoBehaviour
         //transform.position = position; //Saves the position to the current one =(
         rubyRB2D.MovePosition(position);
 
-        Debug.Log("horizontal" + horizontal); //See the values are you sending when pressing the key :))))
+        Debug.Log("horizontal" + horizontal); //See the values are you sending when pressing the key =))))
         Debug.Log("vertical" + vertical);
+    }
+
+    public void ChangeHealth (int amount) 
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth); //Limits the number between 0 and the max health =x
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
