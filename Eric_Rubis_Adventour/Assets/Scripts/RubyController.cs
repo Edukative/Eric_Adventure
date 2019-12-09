7a02ca47bc8268f8cdb981ea7ad7b4fa8163babc
Edukative <56083723+Edukative@users.noreply.github.com>
2019-12-09 18:31:45 +0100

    Ns LOL

--- Eric_Rubis_Adventour/Assets/Scripts/RubyController.cs
+++ Eric_Rubis_Adventour/Assets/Scripts/RubyController.cs
@@ -8,6 +8,9 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rubyRB2D;
     public int maxHealth = 5;
     public int currentHealth;
+    public float timeInvicible = 2.0f;
+    bool isInvincible;
+    float InvincibleTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -26,16 +29,33 @@ public class RubyController : MonoBehaviour
 
         position.x = position.x + speed * horizontal * Time.deltaTime; //The position is equal to the same position but a little bit bigger XD
         position.y = position.y + speed * vertical * Time.deltaTime; //Called each second instead of each frame
-
-        //transform.position = position; //Saves the position to the current one =(
+        //transform.position = position; 
+        //Saves the position to the current one =(
         rubyRB2D.MovePosition(position);
 
-        Debug.Log("horizontal" + horizontal); //See the values are you sending when pressing the key =))))
-        Debug.Log("vertical" + vertical);
+        if (isInvincible) //Invincible because the player has collided with damage
+        {
+            InvincibleTimer -= Time.deltaTime; //Count down the timer
+
+            if (InvincibleTimer < 0) //The timer ended
+            {
+                isInvincible = false; //The player is vulnerable again
+            }
+        }
     }
 
     public void ChangeHealth (int amount) 
     {
+        if (amount < 0) //As is nferior to 0, it means damage
+        {
+            if (isInvincible) //Already invinvible? Don't do anything
+            {
+                return;
+            }
+            isInvincible = true; //Make the player invincible
+            InvincibleTimer = timeInvicible; //Resets the timer to the public value
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth); //Limits the number between 0 and the max health =x
         Debug.Log(currentHealth + "/" + maxHealth);
     }
