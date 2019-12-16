7c12c79e50ff559e3469292c21d5f1c6cc0f54d5
Edukative <56083723+Edukative@users.noreply.github.com>
2019-12-16 18:29:12 +0100

    Enemies
    LOL

---
+++ Eric_Rubis_Adventour/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb2D;
+    public bool isVertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = rb2D.position;
+
+        if (isVertical)
+        {
+            position.y = position.y + Time.deltaTime * speed; //Same as isVertical == true; 
+        }
+
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed;
+        }
+
+        rb2D.MovePosition(position);
+    }
+}
