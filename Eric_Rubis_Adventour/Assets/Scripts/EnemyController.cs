5fa62e65f4097f44a84850e388e3ab286ac3fd64
Edukative <56083723+Edukative@users.noreply.github.com>
2020-02-03 18:30:44 +0100

    New Waypoint
    LOL

--- Eric_Rubis_Adventour/Assets/Scripts/EnemyController.cs
+++ Eric_Rubis_Adventour/Assets/Scripts/EnemyController.cs
@@ -16,18 +16,38 @@ public class EnemyController : MonoBehaviour
     //Animator Values
     Animator anim;
 
+    //Waypoint values
+    public Vector2[] localNodes;
+    //.private Vector2[] worldNodes;
+    int currentNode;
+    int nextNode;
+    Vector2 Velocitiy; 
+
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
         anim = GetComponent<Animator>();
+
+        //Waypoint stuff
+        localNodes = new Vector2[transform.childCount];
+
+        for (int i = 0; i <= transform.childCount - 1; ++i)
+        {
+            Transform child = transform.GetChild(i).transform;
+            localNodes[i] = new Vector2(child.transform.position.x, child.transform.position.y);
+            Debug.Log("index " + i + "Transform " + localNodes[i]);
+        }
+
+        currentNode = 0;
+        nextNode = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
+       /*timer -= Time.deltaTime;
 
         if (timer < 0)
         {
@@ -36,8 +56,12 @@ public class EnemyController : MonoBehaviour
         }
 
         Vector2 position = rb2D.position;
+        */
+       
+        Vector2 wayPointDirection = localNodes[nextNode] - rb2D.position;
+        float dist = speed * Time.deltaTime;
 
-        if (isVertical)
+        /* if (isVertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction; //Same as isVertical == true; 
 
@@ -52,10 +76,21 @@ public class EnemyController : MonoBehaviour
             anim.SetFloat("Move X", direction);
             anim.SetFloat("Move Y", 0);
         }
-
+        */
         rb2D.MovePosition(position);
-    }
 
+        if (wayPointDirection.sqrMagnitude < dist * dist)
+        {
+            dist = wayPointDirection.magnitude;
+            currentNode = nextNode;
+            nextNode += 1;
+
+            if (nextNode >= localNodes.Length)
+            {
+                nextNode = 0;
+            }
+        }
+    }
     private void OnCollisionEnter2D(Collision2D other)
     {
         RubyController player = other.gameObject.GetComponent<RubyController>();
